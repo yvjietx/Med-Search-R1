04d4152575c11d14d00657f6d24090dece96c68e
PeterGriffinJin <bowenj4@illinois.edu>
2025-04-07 18:35:41 +0000

    add indexing for ANN and bm25

--- search_r1/search/build_index.sh
+++ search_r1/search/build_index.sh
@@ -4,6 +4,8 @@ save_dir=/the/path/to/save/index
 retriever_name=e5 # this is for indexing naming
 retriever_model=intfloat/e5-base-v2
 
+# change faiss_type to HNSW32/64/128 for ANN indexing
+# change retriever_name to bm25 for BM25 indexing
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python index_builder.py \
     --retrieval_method $retriever_name \
     --model_path $retriever_model \
